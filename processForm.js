f5e010f118ab79aee90e433af783db171e3dbc42
HardDisco <49183778+lars-tps@users.noreply.github.com>
2025-01-02 16:13:55 +0800

    fix broken submit

--- processForm.js
+++ processForm.js
@@ -5,6 +5,18 @@ function processForm() {
     var birthday_month = document.getElementById("birthday_month").value;
     var birthday_year = document.getElementById("birthday_year").value;
 
-    
-}
+    console.log(name);
+    console.log(email);
+    console.log(birthday_day);
+    console.log(birthday_month);
+    console.log(birthday_year);
 
+}
+let detailsForm = document.getElementById("details-form");
+detailsForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+  
+    // handle submit
+    processForm();
+  });
+  
